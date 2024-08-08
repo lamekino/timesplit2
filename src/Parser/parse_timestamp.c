54f04b1992a7bd084fc7154618800d055cbb111e
lamekino <zoey.j.mcbride@gmail.com>
2024-08-07 23:24:44 -0400

    improvments

--- src/Parser/parse_timestamp.c
+++ src/Parser/parse_timestamp.c
@@ -26,8 +26,8 @@ union Timestamp {
 #define TS_FIELDS (sizeof(union Timestamp) / sizeof(TimestampField))
 
 static TimestampField
-parse_digit(wchar_t digit, TimestampField value, int valuelen, int fieldnum) {
-    if (fieldnum != 0 && valuelen >= 2) {
+parse_digit(wchar_t digit, TimestampField value, int valuelen, int fieldno) {
+    if (fieldno != 0 && valuelen >= 2) {
         return -1;
     }
 
@@ -43,48 +43,37 @@ static int
 parse_helper(const wchar_t *line, size_t linelen, size_t linepos,
         const TimestampField *basefield, TimestampField *curfield,
         size_t numdigits) {
+    const size_t pos = linepos;
+    const size_t len = linelen;
+
     const size_t fieldpos = curfield - basefield;
 
-    if (linepos >= linelen || fieldpos >= TS_FIELDS) {
+    if (linepos >= len || fieldpos >= TS_FIELDS) {
         return 0;
     }
 
-    /* and if this where a map of lambdas they'd call it modern! */
-    switch (line[linepos]) {
-    case L'1': case L'2': case L'3': case L'4': case L'5':
-    case L'6': case L'7': case L'8': case L'9': case L'0':
-        goto CLOSURE_DIGIT;
-    case SEP:
-        goto CLOSURE_SEP;
-    default:
-        break;
+    if (iswdigit(line[linepos])) {
+        int nextvalue = parse_digit(line[pos], *curfield, numdigits, fieldpos);
+        if (nextvalue < 0) {
+            return -1;
+        }
+
+        numdigits += 1;
+        linepos += 1;
+        *curfield = nextvalue;
+
+        return parse_helper(line, len, linepos, basefield, curfield, numdigits);
+    }
+
+    if (line[linepos] == SEP) {
+        numdigits = 0;
+        linepos += 1;
+        curfield += 1;
+
+        return parse_helper(line, len, linepos, basefield, curfield, numdigits);
     }
 
     return -1;
-
-CLOSURE_DIGIT: {
-    int nextvalue =
-        parse_digit(line[linepos], *curfield, numdigits, fieldpos);
-
-    if (nextvalue < 0) {
-        return -1;
-    }
-
-    *curfield = nextvalue;
-    linepos += 1;
-    numdigits += 1;
-
-    return parse_helper(line, linelen, linepos, basefield, curfield, numdigits);
-}
-
-CLOSURE_SEP: {
-    curfield += 1;
-    linepos += 1;
-    numdigits = 0;
-
-    return parse_helper(line, linelen, linepos, basefield, curfield, numdigits);
-}
-
 }
 
 static size_t
@@ -92,19 +81,19 @@ parse_ts_field_num(const wchar_t *line, size_t len) {
     const size_t MAX_SEPS = TS_FIELDS - 1;
 
     size_t i;
-    size_t sep_count = 0;
+    size_t sepcount = 0;
 
     for (i = 0; i < len; i++) {
         if (line[i] == SEP) {
-            sep_count++;
+            sepcount++;
         }
     }
 
-    if (!(1 <= sep_count && sep_count <= MAX_SEPS)) {
+    if (!(1 <= sepcount && sepcount <= MAX_SEPS)) {
         return -1;
     }
 
-    return MAX_SEPS - sep_count;
+    return MAX_SEPS - sepcount;
 }
 
 time_t
