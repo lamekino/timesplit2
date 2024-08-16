63ee7ee242ed1c3496572cfd50bb3d9470433432
lamekino <zoey.j.mcbride@gmail.com>
2024-08-16 14:14:08 -0400

    following refactor main

---
+++ src/App/init.c
@@ -0,0 +1,22 @@
+#include <locale.h>
+
+#include "Types/Error.h"
+#include "Types/Stack.h"
+
+union Error
+init(const char *localename, int argc, struct Stack *mem) {
+    if (argc < 2) {
+        return error_level(LEVEL_FAILED);
+    }
+
+    if (!stack_create(mem)) {
+        return error_level(LEVEL_NO_MEM);
+    }
+
+    if (!setlocale(LC_ALL, localename)) {
+        return error_msg("could not set locale to '%s'", localename);
+    }
+
+    return error_level(LEVEL_SUCCESS);
+}
+
---
+++ src/App/init.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Types/Error.h"
+#include "Types/Stack.h"
+
+union Error
+init(const char *localename, int argc, struct Stack *mem);
